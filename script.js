b3c18a11c478b2a823d278646f8f602069232313
rezi93 <103065023+rezi93@users.noreply.github.com>
2022-05-18 23:05:21 +0400

    add js

---
+++ script.js
@@ -0,0 +1,23 @@
+// let array = [12, 25, 3, 6, 8, 14, 7, 23];
+// let newnumbersmap = array.map (function (x) {
+//     return x % 3;
+// });
+
+// let array = ["hello", 125, "javascript", "html", 43, "css", "scss", "bootstrap", 88, 59, "python"];
+// let newsumfilter = array.filter(function (x){
+//     return x == number;
+// });
+
+
+
+// let languages = ['html', 'css', 'javascript', 'python', 'php'];
+
+// let array = languages.filter(item => item.length > 3);
+// console.log(array);
+
+
+let array= ['academy', 'of', 'digital', 'industries'].  reduce (function (x,y){
+    return x.concat(y);
+},
+
+console.log(array);
